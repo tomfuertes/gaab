1290561a90d6304de150352fdea5efb92135e33c
Tom Fuertes <tomfuertes@gmail.com>
2014-02-23 09:25:57 -0600

    checkin null tests

--- test/spec.js
+++ test/spec.js
@@ -1,10 +1,37 @@
-/*global yeoball:false, test:false, ok:false */
+/*global test:false, ok:false */
 
-(function () {
+/*
+Test methods:
+  module(name, {[setup][ ,teardown]})
+  test(name, callback)
+  expect(numberOfAssertions)
+  stop(increment)
+  start(decrement)
+Test assertions:
+  ok(value, [message])
+  equal(actual, expected, [message])
+  notEqual(actual, expected, [message])
+  deepEqual(actual, expected, [message])
+  notDeepEqual(actual, expected, [message])
+  strictEqual(actual, expected, [message])
+  notStrictEqual(actual, expected, [message])
+  throws(block, [expected], [message])
+*/
+
+
+(function (window) {
   'use strict';
 
-  test('Library answer questions with YO!', function () {
-    ok(gaab('Should I tickle this unicorn?') === 'YO!', 'Passed!');
+  test('Library calls window.ga', function () {
+    // window.ga = function (fn) { fn() };
+
+    // this.spy(window, 'ga');
+
+    // gaab('name', 1, [{ name: 'control' }]);
+
+    // debugger;
+
+    ok(true, 'yay!');
   });
 
-}());
+}(this));
